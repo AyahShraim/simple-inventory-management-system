80d85439c6ee09add1ce3914bf5c57504d293d14
Ayah Shraim <ayahshraim558@gmail.com>
2023-12-17 19:55:52 +0200

    Refactor the program file

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
-﻿using SimpleInventoryManagementSystem.ProductsManegements;
+﻿using SimpleInventoryManagementSystem.MenuCommand;
+using SimpleInventoryManagementSystem.ProductsManagement;
+using SimpleInventoryManagementSystem.UI;
+using SimpleInventoryManagementSystem.Utilities;
 
-Inventory Inv=new Inventory();
 PrintWelcome();
+Inventory inventory = new Inventory();
+IWriter writer = new ConsoleWriter();
+InventoryUI inventoryUI =new InventoryUI(inventory, writer);
+MainProgramUI mainProgramUI = new MainProgramUI(inventoryUI);
 ShowMainMenu();
-
-
 void PrintWelcome()
 {
     Console.ForegroundColor = ConsoleColor.Yellow;
@@ -19,25 +23,19 @@ void PrintWelcome()
     Console.WriteLine("Press any key to start!");
     Console.ReadLine();
     Console.Clear();
-
 }
-
 void ShowMainMenu()
 {
-    string? choice;
-    while (true) { 
+    while (true)
+    {
         Console.ForegroundColor = ConsoleColor.White;
         Console.ResetColor();
-        Console.Clear();
-
-        Console.WriteLine($"\nYou currently have {Inventory.products.Count} product");
-
+        Console.WriteLine($"\nYou currently have {inventory.GetAllProducts().Count} product");
         Console.WriteLine(@"
 --------------------------
-Select an action to statrt ->
+Select an action to start ->
 --------------------------
 ");
-
         Console.WriteLine("1.Add product");
         Console.WriteLine("2.View all products ");
         Console.WriteLine("3.Edit product");
@@ -47,42 +45,19 @@ Select an action to statrt ->
         Console.ForegroundColor = ConsoleColor.Yellow;
         Console.WriteLine("Your Choice is:");
         Console.ForegroundColor = ConsoleColor.White;
-        choice = Console.ReadLine();
-
-        switch (choice)
-        {
-            case "1":
-                Inv.AddProduct();
-                Console.ReadLine();
-                break;
-
-            case "2":
-                Inv.ViewAllProducts();
-                Console.ReadLine();
-                break;
-
-            case "3":
-                Inv.UpdateProduct();
-                Console.ReadLine();
-                break;
-
-            case "4":
-                Inv.DeleteProduct();
-                Console.ReadLine();
-                break;
-            case "5":
-                Inv.SearchDisplayProduct();
-                Console.ReadLine();
-                break;
-
-            case "0":
-                Console.WriteLine("Good Bye!");
-                return;
-
-            default:
-                Console.ReadLine();
-                break;
-        }
-    } 
-   
+        HandleuserInput();
+    }
+}
+void HandleuserInput()
+{
+    string userSelection = Console.ReadLine()?.Trim();
+    if (Enum.TryParse(userSelection, out MenuChoice userChoice))
+    {
+        mainProgramUI.HandleUserInput(userChoice);
+    }
+    else
+    {
+        Console.WriteLine("Invalid choice! Please enter a valid option.");
+    }
+    Console.WriteLine("**********************************************");
 }
