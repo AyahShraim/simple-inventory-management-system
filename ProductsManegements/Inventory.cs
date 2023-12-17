dd22b0a040ccd0dfd9c4dff3c5d61e361f28e16e
Ayah Shraim <ayahshraim558@gmail.com>
2023-12-17 19:55:07 +0200

    Refactor the inventory class

--- ProductsManegements/Inventory.cs
+++ ProductsManegements/Inventory.cs
@@ -1,198 +1,51 @@
-﻿using System.Diagnostics;
-using System.Reflection;
-using System.Security.Cryptography;
-using System.Text.RegularExpressions;
-using System.Xml.Linq;
-
-namespace SimpleInventoryManagementSystem.ProductsManegements
+﻿namespace SimpleInventoryManagementSystem.ProductsManagement
 {
-    internal class Inventory
+    public class Inventory
     {
-        internal static List<Product> products =new();
-
-        public void AddProduct()
+        private readonly List<Product> products = new List<Product>();
+        public bool AddProduct(Product product)
         {
-            string name = " ";
-            decimal price = 0;
-            int quantity = 0;
-            Product? product=null;
-
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("Enter the name of the product: ");
-            Console.ResetColor();
-            name = Console.ReadLine() ?? string.Empty;
-            do
+            if (!IsExistProduct(product.Name))
             {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Enter the price of the product: ");
-                Console.ResetColor();
-
-            } while (!decimal.TryParse(Console.ReadLine(), out price));
-            do
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Enter the quantity of the product: ");
-                Console.ResetColor();
-
-            } while (!int.TryParse(Console.ReadLine(), out quantity));
-
-            product=new Product(name, price, quantity);
-            products.Add(product);
-
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"A new product {product.Name} is added successfully! ");
-            Console.ResetColor();
+                products.Add(product);
+                return true;
+            }
+            return false;
         }
-
-        public void ViewAllProducts()
+        private bool IsExistProduct(string name)
         {
-           
-            foreach(Product product in products)
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("-----------------------");
-                Console.ResetColor();
-                Console.WriteLine(product.ShowProductDetails());
-       
-            }
-
-
+            return products.Any(product => product.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
         }
-
-        public Product? SearchProduct()
+        public bool DeleteProduct(string name)
         {
-            Console.ForegroundColor= ConsoleColor.Green;
-            Console.WriteLine("Enter the product name:");
-            Console.ResetColor();
-            string ?name =Console.ReadLine();
-            return products.Find(x => x.Name == name);
-
+            Product existingProduct = SearchProduct(name);
+            if (existingProduct != null)
+            {
+                products.Remove(existingProduct);
+                return true;
+            }  
+                return false;     
         }
-
-        public void SearchDisplayProduct()
+        public bool UpdateProduct(Product existingProduct, Product updatedProduct)
         {
-            Product? product = SearchProduct();
-         
-            if (product != null)
+            if (existingProduct != null)
             {
-                Console.WriteLine(product.ShowProductDetails());
+                int productIndex = products.IndexOf(existingProduct);
+                products[productIndex] = updatedProduct;
+                return true;
             }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("No such product! Recheck the name and try again!");
-                Console.ResetColor();
-            }
+            return false;          
         }
-
-        public void DeleteProduct()
+        public Product? SearchProduct(string name)
         {
-            Product? product = SearchProduct();
-
-            if (product != null)
-            {
-                products.Remove(product);
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Product deleted Sucessfully!");
-                Console.ResetColor();
-            }
-            else
-            {
-
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("No such product! Recheck the name and try again!");
-                Console.ResetColor();
-            }
+            return products.FirstOrDefault(product => product.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
         }
-
-        public void UpdateProduct()
+        public List<Product> GetAllProducts()
         {
-            Product? product=SearchProduct();
-            //string pattern = @"^(?![YyNn]$).*$";
-            int input =0;
-            if (product != null)
-            {
-               void menu()
-                {
-                    Console.WriteLine(@"
-------------------------
-Choose what to modify ->
-------------------------
-");
-                    int i = 1;
-                    foreach (PropertyInfo p in product.GetType().GetProperties())
-                    {
-                        if (p.Name == "Id") continue;
-                        Console.WriteLine($"{i}.Edit {p.Name}");
-                        i++;
-                    }
-                    Console.WriteLine("0.Finish editing ");
-                }
-                while (true)
-                {
-                    menu();
-                    if (int.TryParse(Console.ReadLine(), out input))
-                    {
-                        switch (input)
-                        {
-                            case 1:
-                                string name = Console.ReadLine() ?? string.Empty;
-                                product.Name = name;
-                                Console.ForegroundColor = ConsoleColor.Green;
-                                Console.WriteLine("Product name edited successfully! ");
-                                Console.ResetColor();
-                                break;
-
-                            case 2:
-                                decimal price;
-                                do
-                                {
-                                    Console.WriteLine("Enter valid price: ");
-
-                                } while (!decimal.TryParse(Console.ReadLine(), out price));
-                                product.Price = price;
-                                Console.ForegroundColor= ConsoleColor.Green;
-                                Console.WriteLine("Price edited successfully! ");
-                                Console.ResetColor();
-
-                                break;
-
-                            case 3:
-                                int quantity;
-                                do
-                                {
-                                    Console.WriteLine("Enter the quantity of the product: ");
-
-                                } while (!int.TryParse(Console.ReadLine(), out quantity));
-                                product.Quantity = quantity;
-                                Console.ForegroundColor = ConsoleColor.Green;
-                                Console.WriteLine("Quantity edited successfully! ");
-                                Console.ResetColor();
-                                break;
-                            case 0:
-                                return;
-
-                            default:
-
-                                Console.WriteLine("Not Valid");
-                                break;
-
-
-                        }
-
-                    }
-                }
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("No such product! Recheck the name and try again!");
-                Console.ResetColor();
-            }
-
-
+            return products;
         }
-
-
     }
 }
+      
+
+
