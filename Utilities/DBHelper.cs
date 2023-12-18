7bc38aa08414d65b8f2e2ee9d86b9ce9c22c0f94
Ayah Shraim <ayahshraim558@gmail.com>
2023-12-18 12:27:35 +0200

    add app.config and DbHelper to set the DB connection string

---
+++ Utilities/DBHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+
+namespace SimpleInventoryManagementSystem.Utilities
+{
+    public static class DBHelper
+    {
+        public static string ConnectionString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        } 
+    }
+}
