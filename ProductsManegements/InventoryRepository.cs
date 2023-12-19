6d2b8eb72faf753a8b2c03d307bcbaca2b7097c6
Ayah Shraim <ayahshraim558@gmail.com>
2023-12-19 14:42:26 +0200

    implement edit product in mongodb

--- ProductsManegements/InventoryRepository.cs
+++ ProductsManegements/InventoryRepository.cs
@@ -33,5 +33,25 @@ namespace SimpleInventoryManagementSystem.ProductsManagement
             var result = _productCollection.DeleteOne(filter);
             return result.DeletedCount > 0;          
         }
+        public bool UpdateProduct(string oldName, Product updatedProduct)
+        {
+            var filter = Builders<Product>.Filter.Eq(product => product.Name, oldName);
+            if (CouldUpdateProduct(updatedProduct, oldName))
+            {
+                var update = Builders<Product>.Update
+                    .Set("Name", updatedProduct.Name)
+                    .Set("Quantity", updatedProduct.Quantity)
+                    .Set("Price", updatedProduct.Price)
+                    .Set("Currency", updatedProduct.Currency.ToString());
+                     
+                var result = _productCollection.UpdateOne(filter, update);
+                return result.MatchedCount > 0;
+            }
+            return false;
+        }
+        private bool CouldUpdateProduct(Product updatedProduct, string oldName)
+        {
+            return !IsExistProduct(updatedProduct.Name) || updatedProduct.Name.Equals(oldName);
+        }
     }
 }
